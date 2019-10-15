04fa0e3e401b678c89a5da551e8f8ddab5eb8614
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-10-15 16:42:42 -0300

    Implements First Reverse

---
+++ challenges/first-reverse/README.md
@@ -0,0 +1,4 @@
+# First Reverse
+
+**Dificult:** Easy
+**Description:** For this challenge you will be reversing a string.
---
+++ challenges/first-reverse/index.js
@@ -0,0 +1,4 @@
+const FirstReverse = str => str.split('').reverse().join('');
+
+// keep this function call here
+FirstReverse("Argument goes here") //?
